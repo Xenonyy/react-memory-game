27d70c969bfc5b0fe58cdb7d9101bb158a883343
Xenonyy <gonda.armand@gmail.com>
2025-07-20 00:56:10 +0200

    feat: add customizable card svg

--- src/components/MemoryCards/Card.tsx
+++ src/components/MemoryCards/Card.tsx
@@ -1,11 +1,11 @@
 import type { FC } from 'react';
 import { Box } from '../common/Box';
-import cardBack from '../../assets/game/card-back.svg';
+import { CardSVG } from '../common/cardSVG';
 
 export const Card: FC = () => {
   return (
-    <Box className="w-24 h-36 bg-white rounded-lg p-1">
-      <img src={cardBack} alt="Card Back" />
+    <Box className="w-24 h-36 bg-white rounded-lg p-1 hover:cursor-pointer hover:shadow-lg hover:">
+      <CardSVG />
     </Box>
   );
 };
