fd8dadcaa5f7c7005e9dc0b8161693f825ca615a
Xenonyy <gonda.armand@gmail.com>
2025-07-19 23:25:59 +0200

    feat: add mock layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,32 @@
-import { Title } from '../components/common/Title';
+import { Box } from '../components/common/Box';
+import { Container } from '../components/common/Container';
 import { DefaultLayout } from '../components/layouts/DefaultLayout';
-import { t } from '../messages/messages';
+import logo from '../assets/logo.svg';
+import type { FC } from 'react';
 
-export const HomePage = () => {
+export const HomePage: FC = () => {
   return (
     <DefaultLayout>
-      <Title title={t.homePage.title} />
+      <Container className="bg-white p-20 font-gilroy font-extrabold">
+        <Box className="w-full h-full justify-between text-black">
+          <img src={logo} alt="Logo" className="w-48 h-48" />
+
+          {/* mock structure */}
+          {/* TODO: countdown timer-matches-mistakes component */}
+          <Box className="flex-row">
+            <p>{90}</p>
+            <Box className="flex-col">
+              <p>{2} matches</p>
+              <p>{3} mistakes</p>
+            </Box>
+          </Box>
+          <Box className="flex-row">
+            <p>Settings</p>
+            <p>|</p>
+            <p>Reset</p>
+          </Box>
+        </Box>
+      </Container>
     </DefaultLayout>
   );
 };
