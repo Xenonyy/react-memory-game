340854b1fd73db74c69651741da0d7641063086a
Xenonyy <gonda.armand@gmail.com>
2025-07-20 18:09:23 +0200

    feat: add animation to Card component for flipping

--- src/components/MemoryCards/Card.tsx
+++ src/components/MemoryCards/Card.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Box } from '../common/Box';
 import { CardSVG } from '../svgs/card';
 
@@ -10,10 +11,16 @@ interface CardProps {
 }
 
 export const Card: FC<CardProps> = ({ id, symbol, isFlipped, onClick }) => {
+  const variants = {
+    hidden: { rotateY: 90, opacity: 0 },
+    visible: { rotateY: 0, opacity: 1 },
+    exit: { rotateY: -90, opacity: 0 },
+  };
+
   return (
     <Box
       id={id}
-      className="w-24 h-36 bg-white rounded-lg p-1 hover:cursor-pointer hover:shadow-lg justify-center"
+      className="relative w-24 h-36 rounded-lg cursor-pointer hover:shadow-lg justify-center [perspective:1000px]"
       onClick={() => onClick(id)}
       role="button"
       aria-pressed={isFlipped}
@@ -25,13 +32,33 @@ export const Card: FC<CardProps> = ({ id, symbol, isFlipped, onClick }) => {
         }
       }}
     >
-      {!isFlipped ? (
-        <CardSVG />
-      ) : (
-        <Box className="w-24 h-32 bg-linear-to-r/decreasing from-gradient-start to-gradient-end rounded-lg m-1 justify-center flex items-center">
-          <p className="text-6xl select-none">{symbol}</p>
-        </Box>
-      )}
+      <AnimatePresence initial={false}>
+        {!isFlipped ? (
+          <motion.div
+            key="front"
+            className="absolute inset-0 backface-hidden transform-3d flex items-center justify-center bg-white rounded-lg p-1 shadow-lg"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            transition={{ duration: 0.3 }}
+          >
+            <CardSVG />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="back"
+            className="absolute backface-hidden transform-3d inset-0 flex items-center justify-center bg-linear-to-r/decreasing from-gradient-start to-gradient-end rounded-lg shadow-lg"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            transition={{ duration: 0.3 }}
+          >
+            <p className="text-6xl select-none">{symbol}</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </Box>
   );
 };
