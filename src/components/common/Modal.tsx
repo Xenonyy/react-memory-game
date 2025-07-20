c5723ff2ee0638d65f5cb32a2b165df3f72487d8
Xenonyy <gonda.armand@gmail.com>
2025-07-20 16:37:03 +0200

    style: apply dynamic classes based on results

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -3,6 +3,7 @@ import { messages } from '../../messages/messages';
 import { Box } from './Box';
 import { CloseSVG } from '../svgs/close';
 import type { ModalTypes } from '../../types/modalTypes';
+import clsx from 'clsx';
 
 interface ModalProps {
   onClick?: () => void;
@@ -13,13 +14,20 @@ interface ModalProps {
 export const Modal: FC<ModalProps> = ({ onClick, children, type }) => {
   const t = messages.modal[type];
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/70">
+    <div className="fixed inset-0 flex items-center justify-center bg-black/70 z-10">
       <div className="bg-white rounded-3xl shadow-lg">
-        <Box className="flex-row mb-4 justify-between bg-primary-bg px-5 py-7 rounded-3xl rounded-b-none">
-          <h2 className="text-xl pr-4">{t.title}</h2>
+        <Box className="flex-row mb-4 justify-between bg-primary-bg p-5 rounded-3xl rounded-b-none">
+          <h2
+            className={clsx('text-xl pr-4', {
+              ['text-secondary-text']: type === 'defeat' || type === 'timeup',
+              ['text-blue-400']: type === 'victory',
+            })}
+          >
+            {t.title}
+          </h2>
           <CloseSVG onClick={onClick} />
         </Box>
-        {children}
+        <Box className="px-7 mb-4 flex-col">{children}</Box>
       </div>
     </div>
   );
